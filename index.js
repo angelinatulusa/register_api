963c11e8434423234b67cf5085a5b7b715116d8e
Opilane TTHK <opilane@tthk.ee>
2023-12-05 09:34:28 +0200

    väljund ekraanil

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/kasutajad', (req,res) => {
+    res.send(['1kasutaja', '2kasutaja'])
+})
+
 app.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
