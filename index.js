d8d1f1f5b226e5d0020990eec6c67c9a842cbb4a
Zara <Zara@DESKTOP-R22G3DK>
2023-12-10 22:15:05 +0200

    done

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 const kasutajad = [
-    { id: 1, nimi: "1kasutaja", tootajatekood: 12345, aeg: '12:45' },
-    { id: 2, nimi: "2kasutaja", tootajatekood: 12122, aeg: '11:27' }
+    { id: 1, nimi: "1kasutaja", tootajatekood: 12345, aeg: '2023-03-17 09:23:45' },
+    { id: 2, nimi: "2kasutaja", tootajatekood: 12122, aeg: '2023-05-22 14:36:21' },
+    {id: 3, nimi: "3kasutaja", tootajatekood: 11221, aeg: '2023-08-01 18:45:12'},
+    { id: 4, nimi: "Alice", tootajatekood: 67890, aeg: "2023-04-10 07:30:00" },
+    { id: 5, nimi: "Bob", tootajatekood: 54321, aeg: "2023-09-05 22:11:55" },
+    { id: 6, nimi: "Charlie", tootajatekood: 98765, aeg: "2023-01-28 11:05:33" },
+    { id: 7, nimi: "Alice", tootajatekood: 12345, aeg: "2023-06-14 15:20:48" },
+    { id: 8, nimi: "Eve", tootajatekood: 67890, aeg: "2023-02-19 05:40:26" },
+    { id: 9, nimi: "Bob", tootajatekood: 54321, aeg: "2023-11-03 20:16:09" },
+    { id: 10, nimi: "Mallory", tootajatekood: 98765, aeg: "2023-07-08 13:55:30" },
+    { id: 11, nimi: "Alice", tootajatekood: 12345, aeg: "2023-10-12 08:02:17" },
+    { id: 12, nimi: "Charlie", tootajatekood: 67890, aeg: "2023-12-01 23:45:00" },
+    { id: 13, nimi: "Eve", tootajatekood: 54321, aeg: "2023-04-05 03:18:57" }
 ];
 
 app.get('/kasutajad', (req, res) => {
@@ -28,13 +39,14 @@ app.get('/kasutajad/:id', (req, res) => {
 });
 
 app.post('/kasutajad', (req, res) => {
-    if (!req.body.name || !req.body.price) {
+    if (!req.body.nimi || !req.body.tootajatekood || !req.body.aeg) {
         return res.status(400).send({ error: 'One or all params are missing' });
     }
     let user = {
         id: kasutajad.length + 1,
-        price: req.body.price,
-        name: req.body.name
+        nimi: req.body.nimi,
+        tootajatekood: req.body.tootajatekood,
+        aeg: req.body.aeg,
     };
     kasutajad.push(user);
 
